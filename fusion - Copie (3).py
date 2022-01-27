ad7a02c4ec80847fe0544af1266ca5af312b4893
[lamine] <lamineelias200@gmail.com>
2022-01-27 23:09:48 +0100

    version1

---
+++ fusion - Copie (3).py
@@ -0,0 +1,16 @@
+import pandas as pd
+#import csv
+import glob
+import os
+
+# merging the files
+files_joined = os.path.join('C:\\SAE-15\\vaccination-covid\\data\\raw', "donnees-*.csv")
+
+# Return a list of all joined files
+list_files = glob.glob(files_joined)
+
+print("** Merging multiple csv files into a single pandas dataframe **")
+# Merge files by joining all files
+dataframe = pd.concat(map(pd.read_csv, list_files), ignore_index=True)
+dataframe.to_csv("fusion.csv", index=False)
+print(dataframe)
