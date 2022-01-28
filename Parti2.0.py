0b722e59231d438ee137573ee9c42137a7e4c815
Elmirbtj <92865807+Elmirbtj@users.noreply.github.com>
2022-01-28 10:59:07 +0100

    Update Parti2.0.py

--- Parti2.0.py
+++ Parti2.0.py
@@ -1,10 +1,11 @@
 import csv
-import re
+
+#permet de separer corectement mes clés  et creation d'une liste et dictionnaire .
 header = "date_reference|semaine_injection|region_residence|libelle_region|departement_residence|libelle_departement|population_insee|classe_age|libelle_classe_age|type_vaccin|effectif_1_inj|effectif_termine|effectif_cumu_1_inj|effectif_cumu_termine|taux_1_inj|taux_termine|taux_cumu_1_inj|taux_cumu_termine|date|effectif_rappel|effectif_cumu_rappel|effectif_rappel_parmi_eligible|effectif_eligible_au_rappel|taux_rappel|taux_cumu_rappel|taux_cumu_rappeleli,2022-01-16".split("|")
 
 dico = {}
 liste_dico=[]
-
+#ouverture de notre fichier et la lecture et rajouter une ligne et a liste .
 with open('fusion.csv',"r") as csvfile:
     reader = csv.DictReader(csvfile,delimiter=";")
     for row in reader:
@@ -15,7 +16,7 @@ def search_by(arg,liste):
     for i in liste:
         l.append(i[arg])
     return l
-
+#permet de voir si y'a une case vide puis ajoute au compteur +1 les valeurs manqauntes.
 def missing_val(data):
     missing=0
     for i in data:
@@ -23,7 +24,7 @@ def missing_val(data):
             missing+=1
     return missing
 
-
+#permet de voir la colone date_referance puis voir si il y'a une valeurs aberante .
 def date_reference(date):
     date = date.replace(",","")
     date = date.replace("-","/")
@@ -32,7 +33,7 @@ def date_reference(date):
         if 2022>=int(date[0]) >= 2020:
             return True
     return False
-
+#permet de voir les valeurs aberantes de la semain_inj .
 def semaine_inj(sem):
     sem = sem.split("-")
     if len(sem[0]) == 4 and len(sem[1]) == 2:
@@ -41,12 +42,7 @@ def semaine_inj(sem):
                 return True
     return False
 
-
-def region_residence(reg):
-    if int(reg) < 0 or int(reg) > 101:
-        return False
-    return True
-
+#Fonction qui reunie tout les region permentant par la suite voir les aberantes.
 def libelle_region(name_reg):
     region = ["Auvergne-Rhône-Alpes", "Bourgogne-Franche-Comté", "Bretagne", "Centre-Val de Loire", "Corse",
               "Grand Est", "Hauts-de-France", "Ile-de-France", "Normandie", "Nouvelle-Aquitaine", "Occitanie",
@@ -56,23 +52,12 @@ def libelle_region(name_reg):
         return True
     return False
 
-def classe_age(age):
-    age = age.split("-")
-    if len(age[0]) == 2 and len(age[1]) == 2:
-        if 100>=int(age[0])>=0:
-            if 100>=int(age[1])>=0:
-                if int(age[0]) == 0 and int(age[1]) == 11:
-                    if int(age[0]) == 75:
-                        return True
-                    return False
-                return True
-    return False
 
 
 for i in header:
    print(missing_val(search_by(i,liste_dico,)))
 
-
+#cherche dans la colone date_reference les valurs fausse precédente c'est la meme chose par la suite on reutilise les fonction pour trouver se qui nmanque et ou l'erreur.
 for i in search_by("date_reference",liste_dico):
     if not date_reference(i):
         print("Voici les dates abbérantes :",i)
@@ -85,11 +70,6 @@ for i in search_by("semaine_injection", liste_dico):
 else:
     print("Il y'a pas de valeurs abbérentes")
 
-for i in search_by("region_residence", liste_dico):
-    if not region_residence(i):
-        print("Voici les régions abbérentes:", i)
-else:
-    print("Il y'a pas de valeurs abbérentes")
 
 
 for i in search_by("libelle_region", liste_dico):
@@ -99,8 +79,5 @@ else:
     print("Il y'a pas de valeurs abbérentes")
 
 
-for i in search_by("classe_age", liste_dico):
-    if not classe_age(i):
-      print("Voici les classes d'ages abbérentes:", i)
-else:
-    print("Il y'a pas de classes d'ages abbérentes")
+
+
